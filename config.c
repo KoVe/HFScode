a5fbf5dc9542b43d35468abd031cee7c16722796
kvheyen <koen.verheyen@intec.ugent.be>
2013-10-14 13:27:45 +0200

    configuration bits are set for PIC32MX120F032B and PIC32MX220F032B

--- config.c
+++ config.c
@@ -3,44 +3,86 @@
  *
  * Make sure the correct PIC32 is selected when starting a new project
  *
- * Do not put these settings in a header file!
- * More generally never put #pragma config something in a header file!
+ * Do not put these settings in a header file!!
+ * More generally never put #pragma config "something" in a header file!
  */
 
-// PIC32MX220F032B Configuration Bit Settings
+#if (__PIC32_FEATURE_SET__ == 120)
+    // PIC32MX120F032B Configuration Bit Settings
 
-#include <p32xxxx.h>
+    #include <p32xxxx.h>
 
-// DEVCFG3
-// USERID = No Setting
-#pragma config PMDL1WAY = OFF           // Peripheral Module Disable Configuration (Allow multiple reconfigurations)
-#pragma config IOL1WAY = OFF            // Peripheral Pin Select Configuration (Allow multiple reconfigurations)
-#pragma config FUSBIDIO = ON            // USB USID Selection (Controlled by the USB Module)
-#pragma config FVBUSONIO = ON           // USB VBUS ON Selection (Controlled by USB Module)
+    // DEVCFG3
+    // USERID = No Setting
+    #pragma config PMDL1WAY = OFF           // Peripheral Module Disable Configuration (Allow multiple reconfigurations)
+    #pragma config IOL1WAY = OFF            // Peripheral Pin Select Configuration (Allow multiple reconfigurations)
+    #pragma config FUSBIDIO = ON            // USB USID Selection (Controlled by the USB Module)
+    #pragma config FVBUSONIO = ON           // USB VBUS ON Selection (Controlled by USB Module)
 
-// DEVCFG2
-#pragma config FPLLIDIV = DIV_2         // PLL Input Divider (2x Divider)
-#pragma config FPLLMUL = MUL_16         // PLL Multiplier (16x Multiplier)
-#pragma config UPLLIDIV = DIV_5         // USB PLL Input Divider (5x Divider)
-#pragma config UPLLEN = OFF             // USB PLL Enable (Disabled and Bypassed)
-#pragma config FPLLODIV = DIV_2         // System PLL Output Clock Divider (PLL Divide by 2)
+    // DEVCFG2
+    #pragma config FPLLIDIV = DIV_2         // PLL Input Divider (2x Divider)
+    #pragma config FPLLMUL = MUL_16         // PLL Multiplier (16x Multiplier)
+    #pragma config FPLLODIV = DIV_2         // System PLL Output Clock Divider (PLL Divide by 2)
 
-// DEVCFG1
-#pragma config FNOSC = PRIPLL           // Oscillator Selection Bits (Primary Osc w/PLL (XT+,HS+,EC+PLL))
-#pragma config FSOSCEN = OFF            // Secondary Oscillator Enable (Disabled)
-#pragma config IESO = OFF               // Internal/External Switch Over (Disabled)
-#pragma config POSCMOD = HS             // Primary Oscillator Configuration (HS osc mode)
-#pragma config OSCIOFNC = OFF           // CLKO Output Signal Active on the OSCO Pin (Disabled)
-#pragma config FPBDIV = DIV_1           // Peripheral Clock Divisor (Pb_Clk is Sys_Clk/1)
-#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor Selection (Clock Switch Disable, FSCM Disabled)
-#pragma config WDTPS = PS1              // Watchdog Timer Postscaler (1:1)
-#pragma config WINDIS = OFF             // Watchdog Timer Window Enable (Watchdog Timer is in Non-Window Mode)
-#pragma config FWDTEN = OFF             // Watchdog Timer Enable (WDT Disabled (SWDTEN Bit Controls))
-#pragma config FWDTWINSZ = WISZ_25      // Watchdog Timer Window Size (Window Size is 25%)
+    // DEVCFG1
+    #pragma config FNOSC = PRIPLL           // Oscillator Selection Bits (Primary Osc w/PLL (XT+,HS+,EC+PLL))
+    #pragma config FSOSCEN = OFF            // Secondary Oscillator Enable (Disabled)
+    #pragma config IESO = OFF               // Internal/External Switch Over (Disabled)
+    #pragma config POSCMOD = HS             // Primary Oscillator Configuration (HS osc mode)
+    #pragma config OSCIOFNC = OFF           // CLKO Output Signal Active on the OSCO Pin (Disabled)
+    #pragma config FPBDIV = DIV_1           // Peripheral Clock Divisor (Pb_Clk is Sys_Clk/1)
+    #pragma config FCKSM = CSDCMD           // Clock Switching and Monitor Selection (Clock Switch Disable, FSCM Disabled)
+    #pragma config WDTPS = PS1              // Watchdog Timer Postscaler (1:1)
+    #pragma config WINDIS = OFF             // Watchdog Timer Window Enable (Watchdog Timer is in Non-Window Mode)
+    #pragma config FWDTEN = OFF             // Watchdog Timer Enable (WDT Disabled (SWDTEN Bit Controls))
+    #pragma config FWDTWINSZ = WISZ_25      // Watchdog Timer Window Size (Window Size is 25%)
 
-// DEVCFG0
-#pragma config JTAGEN = OFF             // JTAG Enable (JTAG Disabled)
-#pragma config ICESEL = ICS_PGx2        // ICE/ICD Comm Channel Select (Communicate on PGEC2/PGED2)
-#pragma config PWP = OFF                // Program Flash Write Protect (Disable)
-#pragma config BWP = OFF                // Boot Flash Write Protect bit (Protection Disabled)
-#pragma config CP = OFF                 // Code Protect (Protection Disabled)
+    // DEVCFG0
+    #pragma config JTAGEN = OFF             // JTAG Enable (JTAG Disabled)
+    #pragma config ICESEL = ICS_PGx2        // ICE/ICD Comm Channel Select (Communicate on PGEC2/PGED2)
+    #pragma config PWP = OFF                // Program Flash Write Protect (Disable)
+    #pragma config BWP = OFF                // Boot Flash Write Protect bit (Protection Disabled)
+    #pragma config CP = OFF                 // Code Protect (Protection Disabled)
+
+#endif
+
+#if (__PIC32_FEATURE_SET__ == 220)
+    // PIC32MX220F032B Configuration Bit Settings
+
+    #include <p32xxxx.h>
+
+    // DEVCFG3
+    // USERID = No Setting
+    #pragma config PMDL1WAY = OFF           // Peripheral Module Disable Configuration (Allow multiple reconfigurations)
+    #pragma config IOL1WAY = OFF            // Peripheral Pin Select Configuration (Allow multiple reconfigurations)
+    #pragma config FUSBIDIO = ON            // USB USID Selection (Controlled by the USB Module)
+    #pragma config FVBUSONIO = ON           // USB VBUS ON Selection (Controlled by USB Module)
+
+    // DEVCFG2
+    #pragma config FPLLIDIV = DIV_2         // PLL Input Divider (2x Divider)
+    #pragma config FPLLMUL = MUL_16         // PLL Multiplier (16x Multiplier)
+    #pragma config UPLLIDIV = DIV_5         // USB PLL Input Divider (5x Divider)
+    #pragma config UPLLEN = OFF             // USB PLL Enable (Disabled and Bypassed)
+    #pragma config FPLLODIV = DIV_2         // System PLL Output Clock Divider (PLL Divide by 2)
+
+    // DEVCFG1
+    #pragma config FNOSC = PRIPLL           // Oscillator Selection Bits (Primary Osc w/PLL (XT+,HS+,EC+PLL))
+    #pragma config FSOSCEN = OFF            // Secondary Oscillator Enable (Disabled)
+    #pragma config IESO = OFF               // Internal/External Switch Over (Disabled)
+    #pragma config POSCMOD = HS             // Primary Oscillator Configuration (HS osc mode)
+    #pragma config OSCIOFNC = OFF           // CLKO Output Signal Active on the OSCO Pin (Disabled)
+    #pragma config FPBDIV = DIV_1           // Peripheral Clock Divisor (Pb_Clk is Sys_Clk/1)
+    #pragma config FCKSM = CSDCMD           // Clock Switching and Monitor Selection (Clock Switch Disable, FSCM Disabled)
+    #pragma config WDTPS = PS1              // Watchdog Timer Postscaler (1:1)
+    #pragma config WINDIS = OFF             // Watchdog Timer Window Enable (Watchdog Timer is in Non-Window Mode)
+    #pragma config FWDTEN = OFF             // Watchdog Timer Enable (WDT Disabled (SWDTEN Bit Controls))
+    #pragma config FWDTWINSZ = WISZ_25      // Watchdog Timer Window Size (Window Size is 25%)
+
+    // DEVCFG0
+    #pragma config JTAGEN = OFF             // JTAG Enable (JTAG Disabled)
+    #pragma config ICESEL = ICS_PGx2        // ICE/ICD Comm Channel Select (Communicate on PGEC3/PGED3)
+    #pragma config PWP = OFF                // Program Flash Write Protect (Disable)
+    #pragma config BWP = OFF                // Boot Flash Write Protect bit (Protection Disabled)
+    #pragma config CP = OFF                 // Code Protect (Protection Disabled)
+
+#endif
